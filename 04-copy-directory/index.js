70d2ad1bedb94eb26e898d85a215462b5ae7446e
Natalia Sorokina <nalya92@list.ru>
2023-05-07 18:02:50 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.stat(path.join(__dirname, 'files-copy'), function(err){
+  if (!err) {
+    fs.readdir (path.join(__dirname, 'files-copy'),function(err, copyFiles) {
+      for (let file of copyFiles) {
+        fs.unlink(path.join(__dirname, 'files-copy', file), err => {
+          if (err)  {
+            console.log(err.message);
+          }
+        });
+      }
+    });
+  }
+
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, function () {
+
+    fs.readdir (path.join(__dirname, 'files'),function(err, files) {
+      for (let file of files) {
+
+        fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
+          if (err)  {
+            console.log(err.message);
+          }
+        });
+      }
+    });
+  });
+});
 
